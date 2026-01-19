67a73fce085356dae5909a546beeae2345ff465f
Vineeth Kumar Puneriya <2303a51256@sru.edu.in>
2026-01-19 16:34:21 +0530

    Add files via upload

---
+++ A1-Monday.py
@@ -0,0 +1,25 @@
+def maxMeetings(meetings):
+    meetings.sort(key=lambda x: x[1])
+
+    count = 0
+    lastEnd = -1
+
+    for start, end in meetings:
+        if start > lastEnd:
+            count += 1
+            lastEnd = end
+
+    return count
+
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    meetings = []
+
+    for _ in range(n):
+        startTime, endTime = map(int, input().split())
+        meetings.append((startTime, endTime))
+
+    print(maxMeetings(meetings))
